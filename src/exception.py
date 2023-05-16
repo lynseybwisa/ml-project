2fe3efea06053a830ad2889a0a592f9374ad1a8c
bwisalynsey <bwisa.lynsey@trustgro.com>
2023-05-16 20:43:12 +0300

    logging and exception handling

--- src/exception.py
+++ src/exception.py
@@ -14,7 +14,6 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_details(error_message,error_detail=error_detail)
-        logging.error(self.error_message)
 
     def __str__(self):
         return self.error_message
